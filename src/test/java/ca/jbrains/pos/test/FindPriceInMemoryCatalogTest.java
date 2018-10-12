49b901fa1124e218ecf81420c8c25806181b9be6
J. B. Rainsberger <me@jbrains.ca>
2018-10-12 09:29:32 +0200

    We confirm that we can find a matching price, even if there are many other barcodes.

--- src/test/java/ca/jbrains/pos/test/FindPriceInMemoryCatalogTest.java
+++ src/test/java/ca/jbrains/pos/test/FindPriceInMemoryCatalogTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 public class FindPriceInMemoryCatalogTest {
@@ -13,6 +14,20 @@ public class FindPriceInMemoryCatalogTest {
 
         InMemoryCatalog catalog = new InMemoryCatalog(Collections.singletonMap(
                 "62746", matchingPrice));
+
+        Assert.assertEquals(matchingPrice, catalog.findPrice("62746"));
+    }
+
+    @Test
+    public void productFoundAmongMany() throws Exception {
+        Price matchingPrice = Price.euroCents(700);
+
+        InMemoryCatalog catalog = new InMemoryCatalog(new HashMap<String, Price>() {{
+            put("not 62746", Price.euroCents(-1));
+            put("definitely not 62746", Price.euroCents(-2));
+            put("62746", matchingPrice);
+            put("obviously not 62746, you idiot", Price.euroCents(-3));
+        }});
 
         Assert.assertEquals(matchingPrice, catalog.findPrice("62746"));
     }
