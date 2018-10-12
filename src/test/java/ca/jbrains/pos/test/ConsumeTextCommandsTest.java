4dcbb4beb8cbbae2a5e05aed74841f6176bb78db
J. B. Rainsberger <me@jbrains.ca>
2018-10-12 15:04:49 +0200

    We can now consume text commands, as long as there are exactly 1 of them.

--- src/test/java/ca/jbrains/pos/test/ConsumeTextCommandsTest.java
+++ src/test/java/ca/jbrains/pos/test/ConsumeTextCommandsTest.java
@@ -3,6 +3,7 @@ package ca.jbrains.pos.test;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.StringReader;
 
@@ -17,9 +18,12 @@ public class ConsumeTextCommandsTest {
                 barcodeScannedListener);
 
         Mockito.verify(barcodeScannedListener).onBarcode("::barcode::");
+        // REFACTOR I think this becomes "verify at most n commands (of any kind, let alone 'barcode')".
+        Mockito.verify(barcodeScannedListener, Mockito.atMost(1)).onBarcode(Mockito.anyString());
     }
 
     // SMELL StringReader is probably too specific
     private void consumeTextCommandsUsingListener(StringReader stringReader, BarcodeScannedListener barcodeScannedListener) throws IOException {
+        barcodeScannedListener.onBarcode(new BufferedReader(stringReader).readLine());
     }
 }
