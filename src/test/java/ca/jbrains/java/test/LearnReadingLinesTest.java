8901d03eb97cf89a3417a7cbe161db2b5afef8dd
J. B. Rainsberger <me@jbrains.ca>
2018-10-13 08:31:30 +0200

    Began using Vavr collections in learning tests.

--- src/test/java/ca/jbrains/java/test/LearnReadingLinesTest.java
+++ src/test/java/ca/jbrains/java/test/LearnReadingLinesTest.java
@@ -1,24 +1,22 @@
 package ca.jbrains.java.test;
 
+import io.vavr.collection.List;
 import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.BufferedReader;
 import java.io.StringReader;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class LearnReadingLinesTest {
     @Test
     public void readOneLineWithoutALineSeparator() throws Exception {
         Assert.assertThat(
                 lines("this is one line of text without a line separator."),
-                CoreMatchers.is(Arrays.asList("this is one line of text without a line separator.")));
+                CoreMatchers.is(List.of("this is one line of text without a line separator.")));
     }
 
     private List<String> lines(String blobOfText) {
-        return new BufferedReader(new StringReader(blobOfText)).lines().collect(Collectors.toList());
+        return List.ofAll(new BufferedReader(new StringReader(blobOfText)).lines());
     }
 }
